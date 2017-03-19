a0d798c4900cce448fdec315065bff90c505e855
fly <pingxxhxj@sina.cn>
2017-03-19 19:03:38 +0800

    debug

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,13 @@
 <template>
-  <div id="app">
-
+  <div id="app" class="assa">
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
+export default {
+  name: 'app'
+};
 </script>
 
 <style>
