caab4ad1374dfc476e74d5da5a55eb924fc6f7c7
fly <pingxxhxj@sina.cn>
2017-03-20 23:10:29 +0800

    edit app vue and add header.uve

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,17 @@
 <template>
-  <div id="app" class="assa">
+  <div>
+    <v-header></v-header>
+    <div class="tab border-1px">
+      <div class="tab-item">
+        <a v-link="{path:'/goods'}">商品</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path:'/ratings'}">评论</a>
+      </div>
+      <div class="tab-item">
+        <a v-link="{path:'/seller'}">商家</a>
+      </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
