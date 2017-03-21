5cce3021d168520277d914ceea105a3aa32b785f
fly <pingxxhxj@sina.cn>
2017-03-21 16:50:28 +0800

    add mixin stylus

--- src/App.vue
+++ src/App.vue
@@ -39,16 +39,12 @@ export default {
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-  @import './common/stylus/mixin.styl'
-
   .app
     .tab
       display: flex
       width:100%
       height: 40px
       line-height: 40px
-      // border-bottom: 1px solid rgba(7, 17, 27, 0.1)
-      border-1px(rgba(7, 17, 27, 0.1));
       .tab-item
         flex: 1
         text-align: center
