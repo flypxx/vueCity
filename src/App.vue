30db5d73ebdcb0523fbfe234090e303628834d33
fly <pingxxhxj@sina.cn>
2017-03-21 10:41:50 +0800

    use router-link replace v-link

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     <v-header></v-header>
     <div class="tab border-1px">
       <div class="tab-item">
-        <a v-link="{path:'/goods'}">商品</a>
+        <router-link to="/goods">商品</router-link>
       </div>
       <div class="tab-item">
-        <a v-link="{path:'/ratings'}">评论</a>
+        <router-link to="/ratings">评论</router-link>
       </div>
       <div class="tab-item">
-        <a v-link="{path:'/seller'}">商家</a>
+        <router-link to="/seller">商家</router-link>
       </div>
     </div>
     <router-view></router-view>
@@ -17,7 +17,7 @@
 </template>
 
 <script>
-import header from './components/header/header.vue';
+import header from './components/header/header';
 import axios from 'axios';
 
 export default {
