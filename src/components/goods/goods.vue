37a87bfca326d18f03a9d72c2d3b40e84f0ec0cd
fly <pingxxhxj@sina.cn>
2017-03-25 15:57:42 +0800

    add Better scroll

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
+    <div class="menu-wrapper" ref="menuWrapper">
       <ul>
         <li v-for="item in goods" class="menu-item">
           <span class="text border-1px">
@@ -9,7 +9,7 @@
         </li>
       </ul>
     </div>
-    <div class="foods-wrapper">
+    <div class="foods-wrapper" ref="foodsWrapper">
       <ul>
         <li v-for="item in goods" class="food-list">
           <h1 class="title">{{item.name}}</h1>
@@ -39,6 +39,7 @@
 </template>
 
 <script>
+import BScroll from 'better-scroll';
 import axios from 'axios';
 
 const ERR_OK = 0;
@@ -58,8 +59,17 @@ export default {
       console.log(res);
       if (res.data.errorno === ERR_OK) {
         this.goods = res.data.data;
+        this.$nextTick(() => {
+          this._initScroll();
+        });
       }
     });
+  },
+  methods: {
+    _initScroll() {
+      this.menuScroll = new BScroll(this.$refs.menuWrapper, {});
+      this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {});
+    }
   }
 };
 </script>
