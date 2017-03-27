226e2de22c79d8ce7ccd7ebd687366e192c1c82d
fly <pingxxhxj@sina.cn>
2017-03-27 13:25:04 +0800

    add cartcontrol component

---
+++ src/components/cartcontrol/cartcontrol.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="cartcontrol">
+
+  </div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      food: Object
+    }
+  };
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+</style>
