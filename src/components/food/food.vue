38f7359c9cc056844a6def70535283d0cde8a2d4
fly <pingxxhxj@sina.cn>
2017-03-30 13:51:02 +0800

    add food component

---
+++ src/components/food/food.vue
@@ -0,0 +1,20 @@
+<template>
+  <div v-show="showFoodDetail" class="food"></div>
+</template>
+<script type="text/ecmascript-6">
+  export default {
+    props: {
+      food: Object
+    }
+  };
+</script>
+<style lang="stylus" rel="stylesheet/stylus">
+  .food
+    position fixed
+    top 0
+    left 0
+    bottom 48px
+    width 100%
+    background-color #fff
+    z-index 30
+</style>
