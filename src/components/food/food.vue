493050e4a9f8de6949933126cc7678ae32d277a4
fly <pingxxhxj@sina.cn>
2017-04-01 10:22:22 +0800

    use cartcontrol component

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,33 +1,69 @@
 <template>
   <transition name="move">
-    <div v-show="showFoodDetail" class="food">
+    <div v-show="showFoodDetail"
+         class="food" ref="food">
       <div class="food-content">
         <div class="image-header">
-          <img :src="food.image" alt="">
-          <div class="back">
+          <img :src="food.image">
+          <div class="back" @click="hideFoodDetail">
             <i class="icon-arrow_left"></i>
           </div>
         </div>
+        <div class="content">
+          <div class="title">
+            {{food.name}}
+          </div>
+          <div class="detail">
+            <span class="sell-count">月售{{food.sellCount}}份</span>
+            <span class="food-rating">好评率{{food.rating}}%</span>
+          </div>
+          <div class="price">
+            <span class="now">￥{{food.price}}</span><span class="old"
+                  v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+          </div>
+        </div>
+        <div class="cartcontrol-wrapper">
+          <cartcontrol :food="food"></cartcontrol>
+        </div>
+        <div class="buy"></div>
       </div>
     </div>
   </transition>
 </template>
 <script type="text/ecmascript-6">
-  export default {
-    props: {
-      food: Object
+import BScroll from 'better-scroll';
+import cartcontrol from 'components/cartcontrol/cartcontrol';
+
+export default {
+  props: {
+    food: Object
+  },
+  data() {
+    return {
+      showFoodDetail: false
+    };
+  },
+  methods: {
+    showFood() {
+      this.showFoodDetail = true;
+      this.$nextTick(() => {
+        if (!this.scroll) {
+          this.scroll = new BScroll(this.$refs.food, {
+            click: true
+          });
+        } else {
+          this.scroll.refresh();
+        }
+      });
     },
-    data() {
-      return {
-        showFoodDetail: false
-      };
-    },
-    methods: {
-      showFood() {
-        this.showFoodDetail = true;
-      }
+    hideFoodDetail() {
+      this.showFoodDetail = false;
     }
-  };
+  },
+  components: {
+    cartcontrol
+  }
+};
 </script>
 <style lang="stylus" rel="stylesheet/stylus">
   .food
@@ -39,7 +75,7 @@
     background-color #fff
     z-index 30
     &.move-enter-active,&.move-leave-active
-      transition all 0.4s linear
+      transition all 0.3s linear
       transform translate3d(0, 0, 0)
     &.move-enter,&.move-leave-active
       transform translate3d(100%, 0, 0)
@@ -63,4 +99,35 @@
           padding 10px
           font-size 20px
           color #fff
+    .content
+      padding 18px
+      .title
+        line-height 14px
+        margin-bottom 8px
+        font-size 14px
+        font-weight 700
+        color rgb(7, 17, 27)
+      .detail
+        margin-bottom 18px
+        line-height 10px
+        height 10px
+        font-size 0
+        .sell-count, .food-rating
+          font-size 10px
+          color rgb(147, 153, 159)
+        .sell-count
+          font-size 12px
+      .price
+        font-weight 700
+        line-height 24px
+        .now
+          margin-right 8px
+          font-size 14px
+          color rgb(240, 20, 20)
+        .old
+          text-decoration line-through
+          font-size 10px
+          color rgb(147, 153, 159)
+
+
 </style>
