e6e50f4141f93e2dc90c0b7ea20f15b6790636bc
fly <pingxxhxj@sina.cn>
2017-04-07 14:42:30 +0800

    add rating-item

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -42,6 +42,22 @@
         <div class="rating">
           <h1 class="title">商品评价</h1>
           <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="food.ratings"></ratingselect>
+          <div class="ratings-wrapper">
+            <ul v-show="food.ratings && food.ratings.length">
+              <li v-for="rating in food.ratings" class="rating-item">
+                <div class="user">
+                  <span class="name">{{rating.username}}</span>
+                  <img :src="rating.avatar" width="12" height="12" class="avatar">
+                </div>
+                <div class="time">{{rating.rateTime}}</div>
+                <p class="text">
+                  <span :class="{'icon-thumb_up':rating.rateType===0,'icon-thumb_down':rating.rateType===1}"></span>
+                  {{rating.text}}
+                </p>
+              </li>
+            </ul>
+            <div v-show="!food.ratings || !food.ratings.length" class="no-rating"></div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/ratingselect/ratingselect.vue
+++ src/components/ratingselect/ratingselect.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="ratingselect">
     <div class="rating-type border-1px">
-      <span class="block positive" :class="selectType === 2 ? 'active' : ''">{{desc.all}}<span class="count">47</span></span>
-      <span class="block positive" :class="selectType === 0 ? 'active' : ''">{{desc.positive}}<span class="count">40</span></span>
-      <span class="block negative" :class="selectType === 1 ? 'active' : ''">{{desc.negative}}<span class="count">7</span></span>
+      <span @click="select(2, $event)" class="block positive" :class="selectType === 2 ? 'active' : ''">{{desc.all}}<span class="count">{{ratings.length}}</span></span>
+      <span @click="select(0, $event)" class="block positive" :class="selectType === 0 ? 'active' : ''">{{desc.positive}}<span class="count">{{positives.length}}</span></span>
+      <span @click="select(1, $event)" class="block negative" :class="selectType === 1 ? 'active' : ''">{{desc.negative}}<span class="count">{{negatives.length}}</span></span>
     </div>
-    <div class="switch" :class="{'on':onlyContent}">
+    <div @click="toggleContent" class="switch" :class="{'on':onlyContent}">
       <span class="icon-check_circle"></span>
       <span class="text">只看内容和评价</span>
     </div>
@@ -42,6 +42,34 @@
             negative: '不满意'
           };
         }
+      }
+    },
+    computed: {
+      positives() {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === POSITIVE;
+        });
+      },
+      negatives() {
+        return this.ratings.filter((rating) => {
+          return rating.rateType === NEGATIVE;
+        });
+      }
+    },
+    methods: {
+      select(type, event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.selectType = type;
+        this.$root.eventHub.$emit('ratingtype.select', type);
+      },
+      toggleContent(event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.onlyContent = !this.onlyContent;
+        this.$root.eventHub.$emit('content.toggle', this.onlyContent);
       }
     }
   };
