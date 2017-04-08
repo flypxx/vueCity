4154ed234682ccbaee40e979599ad17dc56772c5
fly <pingxxhxj@sina.cn>
2017-04-08 22:34:11 +0800

    edit date.js

--- src/common/js/date.js
+++ src/common/js/date.js
@@ -2,4 +2,22 @@ export function dateFormat(time, fmt) {
   if (/(y+)/.test(fmt)) {
     fmt = fmt.replace(RegExp.$1, new Date(time).getFullYear().substr(4 - RegExp.$1.length));
   }
+  let obj = {
+    'M+': time.getMonth() + 1,
+    'd+': time.getDate(),
+    'h+': time.getHours(),
+    'm+': time.getMinutes(),
+    's+': time.getSeconds()
+  };
+  for (let key in obj) {
+    if (new RegExp(`(${key})`).test(fmt)) {
+      let str = obj[key] + '';
+      fmt = fmt.replace(RegExp.$1, (RegExp.$1.length === 1) ? str : padLeftZero(str));
+    }
+  }
+  return fmt;
 };
+
+function padLeftZero(str) {
+  return ('00' + str).substr(str.length);
+}
