f1d6a436b8120e11ef0d35bc003c22e90e456910
fly <pingxxhxj@sina.cn>
2017-04-10 09:55:03 +0800

    ratings style

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -28,7 +28,7 @@
       <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="ratings"></ratingselect>
       <div class="rating-wrapper">
         <ul>
-          <li class="rating-item" v-for="rating in ratings">
+          <li class="rating-item border-1px" v-for="rating in ratings">
             <div class="avatar">
               <img width="28" height="28" :src="rating.avatar">
             </div>
@@ -38,12 +38,12 @@
               </h1>
               <div class="star-wrapper">
                 <star :size="24" :score="rating.score"></star>
-                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}</span>
+                <span class="delivery" v-show="rating.deliveryTime">{{rating.deliveryTime}}分钟送达</span>
               </div>
               <p class="text">{{rating.text}}</p>
               <div class="recommend" v-show="rating.recommend && rating.recommend.length">
                 <span class="icon-thumb_up"></span>
-                <span class="" v-for="item in rating.recommend">
+                <span class="item" v-for="item in rating.recommend">
                   {{item}}
                 </span>
               </div>
@@ -110,6 +110,7 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
+  @import '../../common/stylus/mixin.styl'
   .ratings
     position absolute
     top 174px
@@ -175,4 +176,65 @@
             margin-left 12px
             font-size 12px
             color rgb(147, 153, 159)
+    .rating-wrapper
+      padding 0 18px
+      .rating-item
+        display flex
+        padding 18px 0
+        border-1px(rgba(7, 17, 27, 0.1))
+      .avatar
+        flex 0 0 28px
+        width 28px
+        margin-right 12px
+        img
+          border-radius 50%
+      .content
+        position relative
+        flex 1
+        .name
+          margin-bottom 4px
+          line-height 12px
+          font-size 10px
+          color rgb(7, 17, 27)
+        .star-wrapper
+          margin-bottom 6px
+          font-size 0
+          .star
+            display inline-block
+            margin-right 6px
+            vertical-align top
+          .delivery
+            display inline-block
+            vertical-align top
+            line-height 12px
+            font-size 10px
+            color rgb(147, 153, 159)
+        .text
+          margin-bottom 8px
+          line-height 18px
+          font-size 12px
+          color rgb(7, 17, 27)
+        .recommend
+          line-height 16px
+          font-size 0
+          .icon-thumb_up, .item
+            display inline-block
+            line-height 16px
+            margin 0 8px 4px 0
+            font-size 9px
+          .icon-thumb_up
+            color rgb(0, 160, 220)
+          .item
+            padding 0 6px
+            border 1px solid rgba(7, 17, 27, 0.1)
+            border-radius 1px
+            color rgb(147, 153, 159)
+            background-color #fff
+        .time
+          position absolute
+          top 0
+          right 0
+          line-height 12px
+          font-size 10px
+          color rgb(147, 153, 159)
 </style>
