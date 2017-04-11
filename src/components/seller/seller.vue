1e6702587ee0fec43ddb6f7a2f69b5d86c20a17f
fly <pingxxhxj@sina.cn>
2017-04-11 10:11:24 +0800

    edit seller style

--- src/components/seller/seller.vue
+++ src/components/seller/seller.vue
@@ -3,8 +3,8 @@
     <div class="seller-content">
       <div class="overview">
         <h1 class="title">{{seller.name}}</h1>
-        <div class="desc">
-          <star :size="36" :score="seller.score"></star>
+        <div class="desc border-1px">
+          <star :size="36" :score="seller.score" class="star"></star>
           <span class="text">({{seller.ratingCount}})</span>
           <span class="text">月售{{seller.sellConut}}单</span>
         </div>
@@ -46,5 +46,54 @@
 </script>
 
 <style lang="stylus" rel="stylesheet/stylus">
-
+  @import '../../common/stylus/mixin.styl'
+  .seller
+    position absolute
+    top 174px
+    bottom 0
+    left 0
+    width 100%
+    overflow hidden
+    .overview
+      padding 18px
+      .title
+        margin-bottom 8px
+        line-height 14px
+        font-size 14px
+        color rgb(7, 17, 27)
+      .desc
+        padding-bottom 18px
+        line-height 18px
+        border-1px(rgba(7, 17, 27, 0.1))
+        font-size 0
+        .star
+          display inline-block
+          margin-right 8px
+          vertical-align top
+        .text
+          display inline-block
+          margin-right 12px
+          vertical-align top
+          font-size 10px
+          color rgb(77, 85, 93)
+      .remark
+        display flex
+        padding-top 18px
+        .block
+          flex 1
+          text-align center
+          border-right 1px solid rgba(7, 17, 27, 0.1)
+          :last-child
+            border none
+          h2
+            margin-bottom 4px
+            line-height 10px
+            font-size 10px
+            color rgb(147, 153, 159)
+          .content
+            line-height 24px
+            font-size 10px
+            color rgb(7, 17, 27)
+            .stress
+              font-size 24px
 </style>
